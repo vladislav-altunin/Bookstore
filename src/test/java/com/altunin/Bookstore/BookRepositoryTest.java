4c890f2bd27ae4cb7324b415900760a6f9d344b8
Vladislav Altunin <vlad@Vladislavs-MacBook-Pro.local>
2022-12-04 20:04:16 +0200

    @run with deleted

--- src/test/java/com/altunin/Bookstore/BookRepositoryTest.java
+++ src/test/java/com/altunin/Bookstore/BookRepositoryTest.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -21,7 +20,6 @@ import com.altunin.Bookstore.domain.Book;
 import com.altunin.Bookstore.domain.BookRepository;
 import com.altunin.Bookstore.domain.CategoryRepository;
 
-//@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 //@DataJpaTest
 @SpringBootTest
