c0f1add2c85897df4d7d92bed72aeaf4f49fdd57
Vladislav Altunin <vlad@Vladislavs-MacBook-Pro.local>
2022-12-04 19:57:16 +0200

    annotations and references changed for postgres

--- src/test/java/com/altunin/Bookstore/BookRepositoryTest.java
+++ src/test/java/com/altunin/Bookstore/BookRepositoryTest.java
@@ -2,6 +2,9 @@ package com.altunin.Bookstore;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
 //import java.security.cert.Extension;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,10 +23,11 @@ import com.altunin.Bookstore.domain.CategoryRepository;
 
 //@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest
 public class BookRepositoryTest {
-	@Autowired
-	private TestEntityManager entityManager;
+//	@Autowired
+//	private TestEntityManager entityManager;
 	
 	@Autowired
 	private BookRepository repository;
@@ -34,7 +39,8 @@ public class BookRepositoryTest {
 	@Test
 	public void createBook() throws Exception {
 		Book book = new Book("William Shakespeare", "ISBN-9780192814517", "Othello", 1604, 20, crepository.findByName("Science").get());
-		entityManager.persistAndFlush(book);
+//		entityManager.persistAndFlush(book);
+		repository.save(book);
 		assertThat(book.getId()).isNotNull();
 	}
 	
@@ -42,7 +48,8 @@ public class BookRepositoryTest {
 	@Test
 	public void deleteBook() {
 		Book book = new Book("William Shakespeare", "ISBN-9780192814517", "Othello", 1604, 20, crepository.findByName("Science").get());
-		entityManager.persistAndFlush(book);
+		repository.save(book);
+//		entityManager.persistAndFlush(book);
 		repository.deleteAll();
 		assertThat(repository.findAll()).isEmpty();
 		
@@ -51,10 +58,12 @@ public class BookRepositoryTest {
 	//Find book
 	@Test
 	public void findBook() {
-		Book book = new Book("William Shakespeare", "ISBN-9780192814517", "Othello", 1604, 20, crepository.findByName("Science").get());
-		entityManager.persistAndFlush(book);
-		assertThat(repository.findByTitle("Othello").get().getTitle()).isEqualTo("Othello");
+//		Book book = new Book("William Shakespeare", "ISBN-9780192814517", "Othello", 1604, 20, crepository.findByName("Science").get());
+//		entityManager.persistAndFlush(book); 
+//		assertThat(repository.findByTitle("Othello").get().getTitle()).isEqualTo("Othello");
 		
+		Optional<Book> books = repository.findByTitle("Animal Farm");
+		assertThat(books.get().getAuthor()).isEqualTo("George Orwell");
 	}
 
 }
